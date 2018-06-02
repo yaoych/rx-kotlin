85da6e42ba38fbbfc36aeced9935d7609dc3643b
lzuyao@163.com <lzuyao@163.com>
2018-06-02 11:53:17 +0800

    Initial commit

---
+++ src/main/kotlin/com/onet/Hello.kt
@@ -0,0 +1,6 @@
+package com.onet
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/onet/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.onet
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
